78032be99496af8ee8e549ba8c469699c8278ffa
18thompsona <milkman9985@gmail.com>
2025-01-05 18:20:58 -0500

    Added Ship Tests

--- src/_tests/ship.test.js
+++ src/_tests/ship.test.js
@@ -4,3 +4,17 @@ test('Create Ship and check status', () => {
     let ship = new Ship(3);
     expect(ship.IsSunk()).toBe(false);
 });
+
+test('After ship is hit, shows damage', () => {
+    let ship = new Ship(3);
+    ship.Hit();
+    expect(ship.destroyedSpaces).toBe(1);
+});
+
+test('After ship is destroyed, shows as Destroyed', () => {
+    let ship = new Ship(3);
+    ship.Hit();
+    ship.Hit();
+    ship.Hit();
+    expect(ship.IsSunk()).toBe(true);
+});
