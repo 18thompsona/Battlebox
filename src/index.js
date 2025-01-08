aa3746f834debee56f3f292dc56414a7bac1e86e
18thompsona <milkman9985@gmail.com>
2025-01-07 20:02:46 -0500

    Added style and fucntion to create the visual board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+import Player from "./player";
 import "./style.css";
 
-const Player = require('../player');
+const playerBoard = document.querySelector(".boxes-player");
+const computerBoard = document.querySelector(".boxes-computer");
 
-const player1 = new Player('Human', 10);
-const player2 = new Player('Computer', 10);
+let boardsize = 10;
+const human = new Player('Human', boardsize);
+const computer = new Player('Computer', boardsize);
+
+
+function CreateBoard(parentElement, size){
+    parentElement.innerHTML = '';
+    parentElement.style.display = "grid";
+    parentElement.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    parentElement.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < boardsize*boardsize;i++){
+        const box = document.createElement('div');
+        box.classList.add("box");
+
+        parentElement.appendChild(box);
+    }
+}
+
+CreateBoard(playerBoard, boardsize);
+CreateBoard(computerBoard, boardsize);
